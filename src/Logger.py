74fe23138dc68dd8b6133dcfa5ba2a9f6157f9fd
Gamma120 <felix.albin@telecomnancy.net>
2022-05-08 23:42:21 +0200

    delete Stream logger

--- src/Logger.py
+++ src/Logger.py
@@ -6,10 +6,8 @@ def set_logger(logger_name, log_file, mode='a', level=logging.DEBUG):
     format = '%(asctime)s - %(levelname)s - %(message)s'
     formatter = logging.Formatter(format)
     fileHandler = logging.FileHandler(log_file, mode=mode)
+    
     fileHandler.setFormatter(formatter)
-    streamHandler = logging.StreamHandler()
-    streamHandler.setFormatter(formatter)
-
+    fileHandler.setLevel(level)
     l.setLevel(level)
     l.addHandler(fileHandler)
-    l.addHandler(streamHandler)

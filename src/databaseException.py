f3d010c044d6913669d4b4119a161de4ccd3ed03
Gamma_120 <felix.albin@telecomnancy.net>
2022-08-28 00:04:21 +0200

    add database exceptions

---
+++ src/databaseException.py
@@ -0,0 +1,62 @@
+
+from logging import WARNING, ERROR, INFO
+
+#######################
+#   ERROR EXCEPTION   #
+#######################
+
+class DatabaseError(Exception):
+    """Base class for database exceptions."""
+    
+    def __init__(self, level, message="Error appended in database") -> None:
+        self.message = message
+        self.level = level
+        super().__init__(self.message)
+        
+    def __str__(self) -> str:
+        return super().__str__()
+    
+class RowNotFound(DatabaseError):
+    """Exception raised when no row is found."""
+    
+    def __init__(self, table_name, message="No row matched query.") -> None:
+        self.table_name = table_name
+        self.message = message
+        super().__init__(ERROR, message)
+  
+    def __str__(self) -> str:
+        return f'{self.message} Table : {self.table_name}'
+    
+
+
+
+#########################
+#   WARNING EXCEPTION   #
+#########################
+
+class DatabaseWarning(UserWarning):
+    """Base class for database warnings."""
+
+    def __init__(self, level, message="Warning appended in database") -> None:
+        self.message = message
+        self.level = level
+        super().__init__(self.message)
+
+
+class MultipleValues(DatabaseWarning):
+    """Exception raised when multiple values match.
+
+    Attributes:
+        table_name (String):  table queried
+        rows       (Dict):   dictionnary of rows queried
+        message    (String): explanation of the warining
+    """
+    
+    def __init__(self, table_name, rows, message="Multiple rows match query.") -> None:
+        self.table_name = table_name
+        self.rows = rows
+        self.message = message
+        super().__init__(WARNING, message)
+        
+    def __str__(self) -> str:
+        return f'{self.message} Table : {self.table_name}. Rows : {self.rows}'
